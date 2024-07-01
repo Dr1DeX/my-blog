6b00053baf3ab06fd57302ec462b40d185a86ca5
nesterov-art <nesterov-art@yandex-team.ru>
2024-07-01 19:51:25 +0300

    feature: (Frontend) Add user-sidebar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled, { css, keyframes } from 'styled-components';
 import { useAuth } from '../context/AuthContext';
+
 
 const HeaderContainer = styled.header`
     display: flex;
@@ -10,14 +11,16 @@ const HeaderContainer = styled.header`
     padding: 20px;
     background-color: #333;
     color: white;
-    @media (max-width: 768px) {
-        flex-direction: column;
-        align-items: flex-start;
-    }
+    position: relative;
 `;
 
 const Brand = styled.h1`
     margin: 0;
+
+    @media (max-width: 768px) {
+        order: 2;
+        margin-left: 10px;
+    }
 `;
 
 const Nav = styled.nav`
@@ -25,20 +28,7 @@ const Nav = styled.nav`
     align-items: center;
 
     @media (max-width: 768px) {
-        flex-direction: column;
-        width: 100%;
-        max-height: ${props => (props.isOpen ? '300px' : '0')};
-        opacity: ${props => (props.isOpen ? '1' : '0')};
-        visibility: ${props => (props.isOpen ? 'visible' : 'hidden')};
-        transform: ${props => (props.isOpen ? 'translateY(0)' : 'translateY(-20px)')};
-        overflow: hidden;
-        transition: opacity 0.3s ease, transform 0.3s ease, visibility 0.3s ease;
-
-        a {
-            margin: 10px 0;
-            width: 100%;
-            text-align: center;
-        }
+        display: none;
     }
 
     a {
@@ -46,8 +36,6 @@ const Nav = styled.nav`
         margin: 0 10px;
         text-decoration: none;
     }
-
-    
 `;
 
 const Hamburger = styled.div`
@@ -57,11 +45,8 @@ const Hamburger = styled.div`
 
     @media (max-width: 768px) {
         display: flex;
-        position: absolute;
-        top: 20px;
-        right: 20px;
+        order: 1;
     }
-
 `;
 
 const HamburgerBar = styled.div`
@@ -84,64 +69,183 @@ const HamburgerBar = styled.div`
     `}
 `;
 
-const UsernameContainer = styled.span`
+const Sidebar = styled.div`
+    position: fixed;
+    top: 0;
+    height: 100%;
+    width: 250px;
+    background-color: #333;
+    overflow-x: hidden;
+    transition: all 0.5s ease;
+    z-index: 1000;
+    padding-top: 20px;
+
+    ${props => props.position === 'left' ? css`
+        left: ${props.isOpen ? '0' : '-250px'};
+    ` : css`
+        right: ${props.isOpen ? '0' : '-250px'};
+    `}
+`;
+
+const SidebarOverlay = styled.div`
+    display: ${props => (props.isOpen ? 'block' : 'none')};
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 500;
+`;
+
+const SidebarContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 0;
+
+    img {
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        margin-bottom: 10px;
+        cursor: pointer;
+    }
+
+    span {
+        color: white;
+        margin-bottom: 20px;
+    }
+
+    hr {
+        width: 80%;
+        border: 1px solid #444;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+        padding: 10px 20px;
+        width: 100%;
+        text-align: center;
+
+        &:hover {
+            background-color: #444;
+        }
+    }
+`;
+
+const UserProfile = styled.div`
     display: flex;
     align-items: center;
-    margin-right: 10px;
+    cursor: pointer;
 
     img {
         border-radius: 50%;
         width: 40px;
         height: 40px;
-        margin-right: 10px;
+        margin-left: 10px;
     }
 
     @media (max-width: 768px) {
-        img {
-            width: 30px;
-            height: 30px;
-        }
+        display: none;
     }
 `;
 
 const Header = () => {
     const { isAuthenticated, user, logout } = useAuth();
     const [isOpen, setIsOpen] = useState(false);
+    const [sidebarPosition, setSidebarPosition] = useState('left');
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
+        setSidebarPosition('left');
         setIsOpen(!isOpen);
-    }
+    };
 
-    
-    return(
-    <HeaderContainer>
-        <Brand>Techno Dreamer</Brand>
-        <Hamburger onClick={toggleMenu}>
-            <HamburgerBar isOpen={isOpen} />
-            <HamburgerBar isOpen={isOpen} />
-            <HamburgerBar isOpen={isOpen} />
-        </Hamburger>
-        <Nav isOpen={isOpen}>
-            <Link to="/">Главная</Link>
-            <Link to="/posts">Посты</Link>
-            {isAuthenticated ? (
-                <>
-                    <UsernameContainer>
-                        {user?.image && <img src={user.image} alt="user" />}
-                        <span>{user?.username}</span>
-                    </UsernameContainer>
-                    <Link to="/create-post">Создать пост</Link>
-                    <Link to="/" onClick={logout}>Выйти</Link>
-                </>
-            ) : (
-                <>
-                    <Link to="/login">Войти</Link>
-                    <Link to="/register">Регистрация</Link>
-                </>
-            )}
-        </Nav>
-    </HeaderContainer>
-    )
+    const openSidebar = () => {
+        setSidebarPosition('right');
+        setIsOpen(true);
+    };
+
+    const goToProfile = () => {
+        navigate('/profile');
+        setIsOpen(false);
+    };
+
+    const closeSidebar = () => {
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isOpen && !event.target.closest('.sidebar') && !event.target.closest('.hamburger') && !event.target.closest('.user-profile')) {
+                closeSidebar();
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener('click', handleClickOutside);
+        } else {
+            document.removeEventListener('click', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isOpen]);
+
+    return (
+        <HeaderContainer>
+            <Hamburger onClick={toggleMenu} className="hamburger">
+                <HamburgerBar isOpen={isOpen} />
+                <HamburgerBar isOpen={isOpen} />
+                <HamburgerBar isOpen={isOpen} />
+            </Hamburger>
+            <Brand>TechnoDreamer</Brand>
+            <Nav>
+                <Link to="/">Главная</Link>
+                <Link to="/posts">Посты</Link>
+                {isAuthenticated ? (
+                    <>
+                        <Link to="/create-post">Создать пост</Link>
+                        <Link to="/" onClick={logout}>Выйти</Link>
+                        <UserProfile onClick={openSidebar} className="user-profile">
+                            <img src={user?.image} alt="user" />
+                        </UserProfile>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">Войти</Link>
+                        <Link to="/register">Регистрация</Link>
+                    </>
+                )}
+            </Nav>
+            <SidebarOverlay isOpen={isOpen} onClick={closeSidebar} />
+            <Sidebar isOpen={isOpen} className="sidebar" position={sidebarPosition}>
+                <SidebarContent>
+                {isAuthenticated && user?.image && (
+                        <img src={user.image} alt="user" onClick={goToProfile} />
+                    )}
+                    {isAuthenticated && <span>{user?.username}</span>}
+                    <hr />
+                    <Link to="/" onClick={closeSidebar}>Главная</Link>
+                    <Link to="/posts" onClick={closeSidebar}>Посты</Link>
+                    {!isAuthenticated ? (
+                        <>
+                            <Link to="/login" onClick={closeSidebar}>Войти</Link>
+                            <Link to="/register" onClick={closeSidebar}>Регистрация</Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/create-post" onClick={closeSidebar}>Создать пост</Link>
+                            <Link to="/" onClick={() => { logout(); closeSidebar(); }}>Выйти</Link>
+                        </>
+                    )}
+                </SidebarContent>
+            </Sidebar>
+        </HeaderContainer>
+    );
 };
 
 export default Header;
