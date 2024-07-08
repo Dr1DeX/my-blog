575000b0697a6186500f0d6de4685fc46d1a35b7
nesterov-art <nesterov-art@yandex-team.ru>
2024-07-08 16:00:58 +0300

    feature: (Search) Optimizer search-query edge-n-gram method

--- app/search/repository.py
+++ app/search/repository.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+from typing import List
+
 from dataclasses import dataclass
 
 from elasticsearch import AsyncElasticsearch
@@ -9,12 +12,63 @@ from app.search.workers.contracts.consumer.search_schema import DeliveryPostSche
 class ElasticRepository:
     es: AsyncElasticsearch
 
+    def _generate_permutations(self, query: str) -> List[str]:
+        return [''.join(p) for p in permutations(query)]
+
     async def search_posts(self, query: str) -> list[dict]:
         response = await self.es.search(index='posts', body={
             'query': {
-                'multi_match': {
-                    'query': query,
-                    'fields': ['title', 'description', 'author_name', 'category_name']
+                'bool': {
+                    'should': [
+                        {
+                            'multi_match': {
+                                'query': query,
+                                'fields': ['title^3', 'description^2', 'author_name', 'category_name'],
+                                'fuzziness': 'AUTO'
+                            }
+                        },
+                        {
+                            'wildcard': {
+                                'title': {
+                                    'value': f'*{query}*',
+                                    'boost': 2.0
+                                }
+                            }
+                        },
+                        {
+                            'wildcard': {
+                                'description': {
+                                    'value': f'*{query}*',
+                                    'boost': 1.5
+                                }
+                            }
+                        },
+                        {
+                            'wildcard': {
+                                'author_name': {
+                                    'value': f'*{query}*',
+                                    'boost': 1.2
+                                }
+                            }
+                        },
+                        {
+                            'wildcard': {
+                                'category_name': {
+                                    'value': f'*{query}*',
+                                    'boost': 1.0
+                                }
+                            }
+                        }
+                    ],
+                    'minimum_should_match': 1
+                }
+            },
+            'highlight': {
+                'fields': {
+                    'title': {},
+                    'description': {},
+                    'author_name': {},
+                    'category_name': {}
                 }
             }
         })
